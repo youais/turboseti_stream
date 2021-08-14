00e9e0a7968afe754db664b63dad9b6e42bfd4a2
Richard Elkins <richard.elkins@gmail.com>
2021-08-14 16:50:05 -0500

    Update datafiles.py

--- test/datafiles.py
+++ test/datafiles.py
@@ -2,9 +2,7 @@ import os
 import tempfile
 
 here = os.path.dirname(os.path.abspath(__file__))
-
 CFG_FILE = os.path.join(here, "sample_spectra_gen.cfg")
-
 TEMPDIR = tempfile.gettempdir() + "/"
 SYNTH_FILE = TEMPDIR + "turboseti_stream." + str(os.getpid()) + ".fil"
 MAX_DRIFT_RATE = 4.0
